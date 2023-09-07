966d2dd1d3ea879898f1627307ac2339ddda6c53
Rashevmarina <Rashevmarina@gmail.com>
2023-09-07 08:50:48 -0700

    day

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+const galleryEl = document.querySelector("div.gallery");
+
+galleryEl.innerHTML = galleryItemsMarkup;
+galleryEl.addEventListener("click", selectItem);
+
+function selectItem(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
